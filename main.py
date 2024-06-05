2d9627b9ad9649243d5752d9634bafc1d1cd6fda
Yerkanat <sayranbekov.0000@gmail.com>
2024-06-05 21:33:24 +0500

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def main():
     print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
 
 if __name__ == '__main__':
     main()
