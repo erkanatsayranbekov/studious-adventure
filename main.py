6a17539270aeed709536d3663d85cb68cea34f2e
Yerkanat <sayranbekov.0000@gmail.com>
2024-06-05 21:33:43 +0500

    third commit

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ def main():
     print("Good morning, World!")
     print("Good morning, World!")
     print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
+    print("Good morning, World!")
 
 if __name__ == '__main__':
     main()
